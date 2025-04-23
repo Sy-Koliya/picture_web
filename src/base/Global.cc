29468ee4c235ccc38491a27739dcc463d3cf863e
Sy-Koliya <3092496922@qq.com>
2025-04-23 20:07:01 +0800

    add http parser global vars

--- src/base/Global.cc
+++ src/base/Global.cc
@@ -3,6 +3,6 @@
 Global::Global(){
     vars_["WorkPool"] = (size_t)8;
     vars_["SocketPool"] = (size_t)4;
-
-
+    vars_["/test"]/*= func_ptr*/;
+    vars_["Content_length_type"]=std::string("Content-Length");
 }
