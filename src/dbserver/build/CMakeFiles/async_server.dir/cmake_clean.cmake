eac9d5a38bb2e13d1c39cd308e7df976654f02e8
Sy-Koliya <3092496922@qq.com>
2025-04-24 20:36:52 +0800

    add dbserver

---
+++ src/dbserver/build/CMakeFiles/async_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/async_server.dir/async_server.cc.o"
+  "CMakeFiles/async_server.dir/async_server.cc.o.d"
+  "async_server"
+  "async_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/async_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
